b4e9e1f766eaf6ae9761821bbd4b649d0c031a3a
Flores Juan Gabriel <gabriel.flores.tec@gmail.com>
2023-10-23 23:43:30 -0300

    estructura terminada

--- src/routes/About/About.jsx
+++ src/routes/About/About.jsx
@@ -1,9 +1,30 @@
-import './About.css';
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <div>About Screen</div>
-  )
-}
+    <div className="about-container">
+      <header>
+        <h1>Acerca del juego</h1>
+      </header>
+      <section className="synopsis-section">
+        <h2>Historia del Juego</h2>
+        <p>Aquí va la sinopsis del juego...</p>
+      </section>
+      <section className="features-section">
+        <h2>Características principales</h2>
+        <div className="feature-box">
+          <p>Aquí van las características del juego...</p>
+        </div>
+        <div className="feature-box">
+          <p>Aquí van las características del juego...</p>
+        </div>
+        <div className="feature-box">
+          <p>Aquí van las características del juego...</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
 export default About;
