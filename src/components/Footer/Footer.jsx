43802f12a34bfcaea6ac18e4d166506a81a366de
JonahGalahad <jonycala23@gmail.com>
2023-10-26 00:40:12 -0300

    Correcciones y organización de carpetas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Footer.css';
-import gmail from '../../assets/logo_gmail.png';
-import insta from '../../assets/logo_instagram.png';
-import tiktok from '../../assets/logo_tiktok.png';
-import twitter from '../../assets/logo_twitter_x.png';
+import gmail from '../../assets/Social_media_icon/logo_gmail.png';
+import insta from '../../assets/Social_media_icon/logo_instagram.png';
+import tiktok from '../../assets/Social_media_icon/logo_tiktok.png';
+import twitter from '../../assets/Social_media_icon/logo_twitter_x.png';
 
 const Footer = () => {
   return (
@@ -14,15 +14,15 @@ const Footer = () => {
       <div className='footer-social-media'>
         <h4>Contact Us: </h4>
         <div className='social-media'>
-          <a href='https://www.google.com/intl/es/gmail/about/'><img src={gmail} alt='Gmail'/></a>
-          <a href='https://www.instagram.com/'><img src={insta} alt='Instagram'/></a>
-          <a href='https://www.tiktok.com/login?lang=es&redirect_url=https%3A%2F%2Fwww.tiktok.com%2Fupload%3Flang%3Des'><img src={tiktok} alt='TikTok'/></a>
-          <a href='https://twitter.com/?lang=es'><img src={twitter} alt='Twitter'/></a>
+          <a href=''><img src={gmail} alt='Gmail'/></a>
+          <a href=''><img src={insta} alt='Instagram'/></a>
+          <a href=''><img src={tiktok} alt='TikTok'/></a>
+          <a href=''><img src={twitter} alt='Twitter'/></a>
         </div>
       </div>
       <div className='footer-copyright'>
         <p>
-          @{new Date().getFullYear()} NameInc. All right reserved
+          @{new Date().getFullYear()} NameTeamInc. All right reserved
         </p>
       </div>
     </footer>
