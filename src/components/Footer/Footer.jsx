74bc976890e2c8a8b3f2d5054aed2d1be5a8dc98
JonahGalahad <jonycala23@gmail.com>
2023-12-27 18:03:14 -0300

    footer change #1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+//import {Footer} from 'flowbite-react';
 import './Footer.css';
 import gmail from '../../assets/SocialMediaFooter/logo_gmail.png';
 import insta from '../../assets/SocialMediaFooter/logo_instagram.png';
@@ -8,21 +9,30 @@ import twitter from '../../assets/SocialMediaFooter/logo_twitter_x.png';
 const Footer = () => {
   return (
     <footer>
-      <div className='footer-name'>
-        <h3>Phujllay Team</h3>
+      <div className='footer-logo'>
+        <img src='' alt='LOGO'/>
       </div>
-      <div className='footer-social-media'>
-        <h4>Contact Us: </h4>
-        <div className='social-media'>
-          <a href=''><img src={gmail} alt='Gmail' /></a>
-          <a href=''><img src={insta} alt='Instagram' /></a>
-          <a href=''><img src={tiktok} alt='TikTok' /></a>
-          <a href=''><img src={twitter} alt='Twitter' /></a>
+      <div className='line'></div>
+      <div className='footer'>
+        <div className='footer-legal'>
+          <a href=''>Terminos y Condiciones</a>
+          <a href=''>Politica de Privacidad</a>
         </div>
+        <div className='footer-follow'>
+          <h4>Contact Us: </h4>
+          <div className='social-media'>
+            <a href=''><img src={gmail} alt='Gmail' /></a>
+            <a href=''><img src={insta} alt='Instagram' /></a>
+            <a href=''><img src={tiktok} alt='TikTok' /></a>
+            <a href=''><img src={twitter} alt='Twitter' /></a>
+          </div>
+        </div>
+        
+        
       </div>
       <div className='footer-copyright'>
         <p>
-          @{new Date().getFullYear()} NameTeamInc. All right reserved
+          @{new Date().getFullYear()} AndinosGames. All right reserved
         </p>
       </div>
     </footer>
