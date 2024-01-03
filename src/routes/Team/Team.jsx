1acf44b960b02beac07e8879adf984cc5d89c61f
Evelyn Ramos <eveh.rms7@gmail.com>
2024-01-03 10:06:13 -0300

    Update redes Team

--- src/routes/Team/Team.jsx
+++ src/routes/Team/Team.jsx
@@ -1,5 +1,6 @@
 import './Team.css';
 import React from 'react';
+import { FaLinkedin, FaEnvelope, FaGithub, FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';  
 import teamData from '../../utils/teamData.js';
 
 
@@ -8,11 +9,40 @@ const TeamMember = ({ member }) => (
     <img className="image_perfil" src={member.img} alt={member.nombreCompleto} />
     <h2>{member.nombreCompleto}</h2>
     <p>{member.rol}</p>
-    <p>{member.descripcion}</p>
+
     <div className="contact-card">
-      {member.linkedin && <a href={member.linkedin}>LinkedIn</a>}
-      {member.email && <a href={`mailto:${member.email}`}>Email</a>}
-      {member.github && <a href={member.github}>GitHub</a>}
+
+    {member.linkedin && (
+        <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+      )}
+      {member.email && (
+        <a href={`mailto:${member.email}`} target="_blank" rel="noopener noreferrer">
+          <FaEnvelope />
+        </a>
+      )}
+      {member.github && (
+        <a href={member.github} target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+      )}
+      {member.instagram && (
+        <a href={member.instagram} target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+      )}
+      {member.facebook && (
+        <a href={member.facebook} target="_blank" rel="noopener noreferrer">
+          <FaFacebook />
+        </a>
+      )}
+      {member.twitter && (
+        <a href={member.twitter} target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+      )}
+      
     </div>
   </div>
 );
