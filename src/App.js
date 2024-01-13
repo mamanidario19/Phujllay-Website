7f79769cd23cb59a4d3bd07d032bba919b4e53e6
Evelyn Ramos <eveh.rms7@gmail.com>
2024-01-12 22:23:36 -0300

    cambios locales antes de rever

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './routes/Home/Home';
-import About from './routes/About/About';
-import Development from './routes/Development/Development';
-import Roadmap from './routes/Roadmap/Roadmap';
-import Team from './routes/Team/Team';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Development' element={<Development />} />
-        <Route path='/Roadmap' element={<Roadmap />} />
-        <Route path='/Team' element={<Team />} />
-      </Routes>
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
