c98f4bb7a2b2b96013aafd04c4d92ef04a21f35b
JonahGalahad <jonycala23@gmail.com>
2024-02-09 01:14:50 -0300

    change footer #1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
-import {FaLinkedinIn,FaGithub,FaInstagram,FaFacebookF,FaGooglePlusG,FaTiktok} from "react-icons/fa";
-import {FaXTwitter} from "react-icons/fa6";
+import { FaLinkedinIn, FaGithub, FaInstagram, FaFacebookF, FaGooglePlusG, FaTiktok } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='footer-logo'> <img src='' alt='LOGO'/> </div>
+      <hr className='line' />
+      <div className='footer-logo'> <img src='' alt='LOGO' /> </div>
 
       <div className='footer-media'>
         <div className='footer-follow-media'>
-          <div className='media'> <a href=''> <FaGooglePlusG/> </a> <span>Gmail</span> </div>
-          <div className='media'> <a href=''> <FaInstagram/> </a> <span>Instagram</span> </div>
-          <div className='media'> <a href=''> <FaTiktok/> </a> <span>TikTok</span> </div>
-          <div className='media'> <a href=''> <FaXTwitter/> </a> <span>X</span> </div>
-          <div className='media'> <a href=''> <FaFacebookF/> </a> <span>Facebook</span> </div>
-          <div className='media'> <a href=''> <FaGithub/> </a> <span>GitHub</span> </div>
-          <div className='media'> <a href=''> <FaLinkedinIn/> </a> <span>LinkedIn</span> </div>
-        </div>          
+          <div className='media'> <a href=''> <FaGooglePlusG /> </a> <span>Gmail</span> </div>
+          <div className='media'> <a href=''> <FaInstagram /> </a> <span>Instagram</span> </div>
+          <div className='media'> <a href=''> <FaTiktok /> </a> <span>TikTok</span> </div>
+          <div className='media'> <a href=''> <FaXTwitter /> </a> <span>X</span> </div>
+          <div className='media'> <a href=''> <FaFacebookF /> </a> <span>Facebook</span> </div>
+          <div className='media'> <a href=''> <FaGithub /> </a> <span>GitHub</span> </div>
+          <div className='media'> <a href=''> <FaLinkedinIn /> </a> <span>LinkedIn</span> </div>
+        </div>
         <div className='footer-nav-link'>
           <ul>
             <li><Link>Inicio</Link></li>
@@ -30,7 +31,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='line'></div>
+      <hr className='line' />
 
       <div className='footer-other'>
         <div className='other'> <ul><li><Link>Politica de Privacidad</Link></li></ul> </div>
